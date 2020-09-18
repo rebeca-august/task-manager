30f5b2c024c2024b3f64068f28a9681af3f76e16
rebeca-august <rebeca.cazacu@gmail.com>
2020-09-18 16:28:54 +0100

    Updates

---
+++ precache-manifest.d9744a2e1123a3065273aea61122b603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2083bc66d3af2a76e79c4010f2386854",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "b730da5e6aaf09f103be",
+    "url": "/task-manager/static/css/main.f692623e.chunk.css"
+  },
+  {
+    "revision": "0de1a3a679b7713bba06",
+    "url": "/task-manager/static/js/2.ac9330ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-manager/static/js/2.ac9330ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b730da5e6aaf09f103be",
+    "url": "/task-manager/static/js/main.3a29d567.chunk.js"
+  },
+  {
+    "revision": "189db07743d636aefbc6",
+    "url": "/task-manager/static/js/runtime-main.2b273049.js"
+  }
+]);
